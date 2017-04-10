ec65771e84b72f8b30c8322cfcef459b04a270a6
Fayland Lam <fayland@gmail.com>
2017-04-10 08:22:49 +0800

    0.21

--- lib/Dist/Zilla/Plugin/GitHubREADME/Badge.pm
+++ lib/Dist/Zilla/Plugin/GitHubREADME/Badge.pm
@@ -2,7 +2,7 @@ package Dist::Zilla::Plugin::GitHubREADME::Badge;
 
 use strict;
 use 5.008_005;
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 use Moose;
 use Moose::Util::TypeConstraints qw(enum);
